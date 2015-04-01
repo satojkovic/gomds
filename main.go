64e2b042c4d930530dd964df1a1e0928418e3571
satojkovic <satojkovic@gmail.com>
2015-04-01 23:32:19 +0900

    Calculate distance

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+
+	"math"
+
+	"github.com/skelterjohn/go.matrix"
 )
 
 type Mnist struct {
@@ -48,9 +53,37 @@ func NewMnist(csvFile string) []Mnist {
 	return mnistData
 }
 
+func (src Mnist) distTo(dst Mnist) (dist float64, err error) {
+	dist = 0.0
+	if src.dim != dst.dim {
+		return dist, errors.New("Mismatched: src.dim != dst.dim")
+	}
+
+	m1 := matrix.MakeDenseMatrix(src.data, 1, src.dim)
+	m2 := matrix.MakeDenseMatrix(dst.data, 1, dst.dim)
+	m3, _ := m1.Times(m2.Transpose())
+
+	dist = math.Sqrt(m3.Get(0, 0))
+
+	return dist, nil
+}
+
 func main() {
 	// Read MNIST Data
 	mnist := NewMnist("./train.csv")
 	fmt.Printf("Num of Data: %d\n", len(mnist))
 	fmt.Printf("Num of Dims: %d\n", mnist[0].dim)
+
+	// Calculate distance
+	for i, _ := range mnist {
+		if i == len(mnist)-1 {
+			break
+		}
+
+		dist, err := mnist[i].distTo(mnist[i+1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("dist(mnist[%d], mnist[%d]) = %f\n", i, i+1, dist)
+	}
 }
